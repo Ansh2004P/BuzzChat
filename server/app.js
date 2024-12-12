74c33e099cb226208ebbe4d9ce232a8d581076f8
Ansh2004P <anshkpatel15@gmail.com>
2024-12-12 15:27:31 +0530

    bebugging

--- server/app.js
+++ server/app.js
@@ -1,23 +1,23 @@
-import express from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+import dotenv from "dotenv"
+import path from "path"
+import { fileURLToPath } from "url"
 
-const app = express();
-dotenv.config({ path: "./.env" });
+const app = express()
+dotenv.config({ path: "./.env" })
 
 // Define __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // Configuration options for cors
 const corsOptions = {
     origin: process.env.CORS_ORIGIN || "*",
     methods: ["GET", "POST", "PATCH", "DELETE", "PUT", "OPTIONS"],
     credentials: true,
-};
+}
 
 // Rate limiter
 // const limiter = rateLimit({
@@ -26,29 +26,37 @@ const corsOptions = {
 // });
 // app.use(limiter);
 
-app.use(cors(corsOptions));
+app.use(cors(corsOptions))
 
-app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static("public"));
-app.use(cookieParser());
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
+app.use(express.static("public"))
+app.use(cookieParser())
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.static(path.join(__dirname, "../client/dist")))
 
 // routes Import
-import userRoutes from "./routes/user.routes.js";
-import chatRoutes from "./routes/chat.routes.js";
-import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js"
+import chatRoutes from "./routes/chat.routes.js"
+import messageRoutes from "./routes/message.routes.js"
 
 // routes declaration
-app.use("/api/v1/user", userRoutes);
-app.use("/api/v1/chat", chatRoutes);
-app.use("/api/v1/message", messageRoutes);
+app.use("/api/v1/user", userRoutes)
+app.use("/api/v1/chat", chatRoutes)
+app.use("/api/v1/message", messageRoutes)
 
 // Handle all other routes by serving the index.html file
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
-});
+    if (process.env.NODE_ENV === "production") {
+        // Serve the index.html from the production build
+        // res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+        res.redirect("https://buzzchat-fe.onrender.com/")
+    } else {
+        // Redirect to the frontend development server in local
+        res.redirect("http://localhost:5173") // Replace with your local frontend dev server URL
+    }
+})
 
-export { app, corsOptions };
+export { app, corsOptions }
+// https://buzzchat-fe.onrender.com/
