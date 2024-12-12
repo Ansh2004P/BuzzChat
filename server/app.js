cadbae0c54753d40861e408b60e708dfde8e0817
Ansh2004P <anshkpatel15@gmail.com>
2024-12-12 15:09:19 +0530

    debugging

--- server/app.js
+++ server/app.js
@@ -1,49 +1,54 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
-import dotenv from "dotenv"
-import path from "path"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const app = express()
-dotenv.config({ path: "./.env" })
+const app = express();
+dotenv.config({ path: "./.env" });
+
+// Define __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Configuration options for cors
 const corsOptions = {
-    origin: process.env.CORS_ORIGIN ||  "*",
+    origin: process.env.CORS_ORIGIN || "*",
     methods: ["GET", "POST", "PATCH", "DELETE", "PUT", "OPTIONS"],
     credentials: true,
-}
+};
 
 // Rate limiter
 // const limiter = rateLimit({
 //     windowMs: 15 * 60 * 1000, // 15 minutes
 //     max: 100, // Limit each IP to 100 requests per windowMs
-// })
-// app.use(limiter)
+// });
+// app.use(limiter);
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
-app.use(express.json({ limit: "16kb" }))
-app.use(express.urlencoded({ extended: true, limit: "16kb" }))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, "../client/build")))
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
 // routes Import
-import userRoutes from "./routes/user.routes.js"
-import chatRoutes from "./routes/chat.routes.js"
-import messageRoutes from "./routes/message.routes.js"
+import userRoutes from "./routes/user.routes.js";
+import chatRoutes from "./routes/chat.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 
 // routes declaration
-app.use("/api/v1/user", userRoutes)
-app.use("/api/v1/chat", chatRoutes)
-app.use("/api/v1/message", messageRoutes)
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/chat", chatRoutes);
+app.use("/api/v1/message", messageRoutes);
 
 // Handle all other routes by serving the index.html file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build", "index.html"))
-})
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+});
 
-export { app, corsOptions }
+export { app, corsOptions };
